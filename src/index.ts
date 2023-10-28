16c546e54f519292f40da577023e16d32fa0ee62
Paco Fuentes <pacofuentes.work@gmail.com>
2023-10-28 12:19:59 +0200

    feat: dev scripts and basic server test

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 
-import express from "express"
+import express, { Request, Response } from "express"
 
 const app = express()
 
 app.listen(3000, () => {
   console.log(`Server running`);
 })
+
+app.get('/',(req:Request,res:Response)=>{
+    res.send('Hello world!')
+});
